61cc0493dd47d3a5543f1242861bdc8c60c8c28c
Marc Arndt <marc@marcarndt.com>
2018-11-04 18:30:20 +0200

    adding buildspec and terraform

--- src/views/career.vue
+++ src/views/career.vue
@@ -22,10 +22,10 @@
         </v-timeline-item>
         <v-timeline-item color="cyan ">
             <span slot="opposite" class="headline font-weight-bold cyan--text">
-            2005-2005
+            2005-2010
             </span>
             <v-card class="elevation-2">
-                <v-card-title class="headline cyan--text ">Standard Bank</v-card-title>
+                <v-card-title class="headline cyan--text ">Standard Bank - Java & SAP Developer</v-card-title>
                 <v-card-text>
                     In my final year of university, I applied for the Standard Bank Graduate Development program and was
                     accepted. I moved up to johannesburg to start my new career. Initially, I was placed with the
@@ -59,36 +59,95 @@
             </v-card>
         </v-timeline-item>
         <v-timeline-item color="cyan">
-            <span slot="opposite" class="headline font-weight-bold cyan--text">2005-2008</span>
+            <span slot="opposite" class="headline font-weight-bold cyan--text">2010-2011</span>
             <v-card class="elevation-2">
-                <v-card-title class="headline cyan--text">African Bank</v-card-title>
+                <v-card-title class="headline cyan--text">African Bank - Senior Java Developer</v-card-title>
                 <v-card-text>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
-                    convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
-                    salutandi sententiae.
+                    I went through a divorce, and sadly my then ex wife also worked at Standard Bank, so I decided to
+                    find an opportunity outside of Standard Bank. I joined African Bank as a Senior Java developer,
+                    taking over development on their custom in house web application used by their branch staff to
+                    service their customers. I also spear headed a new system for car dealers to offer loans to client
+                    when purchasing a vehicle.<br/>
+                    <br/>
+                    I was not happy with the business practices of the organization, I did not feel like the work I was
+                    doing was in the best interest of the customers, so I decided to leave the organization.
+                    Approximately a year later the company was bankrupt due to their lending practices.
                 </v-card-text>
             </v-card>
         </v-timeline-item>
 
         <v-timeline-item color="cyan">
-            <span slot="opposite" class="headline font-weight-bold cyan--text">2008-2012</span>
+            <span slot="opposite" class="headline font-weight-bold cyan--text">2011-2014</span>
             <v-card class="elevation-2">
-                <v-card-title class="headline cyan--text">First National Bank</v-card-title>
+                <v-card-title class="headline cyan--text">First National Bank - Senior Java / Tibco BW Developer and
+                    Scrum Master
+                </v-card-title>
                 <v-card-text>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
-                    convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
-                    salutandi sententiae.
+                    I joined First National Bank as a senior Java developer on their CIB team. My first project was to
+                    integrate the smart cash boxes into the banks accounting systems. As soon as a company deposited
+                    money into their cash box, it would automatically reconcile with their bank account. The challenge
+                    was rather interesting since the cash boxes used a proprietary TCP communication stream, so I was
+                    able to build annotated POJO objects to automatically transform the data to and from the required
+                    formats.<br/>
+                    <br/>
+                    While a Java developer, we started using the <a
+                        href="https://en.wikipedia.org/wiki/Personal_software_process">Personal Software Process</a> and
+                    <a href="https://en.wikipedia.org/wiki/Team_software_process">Team Software Process</a>
+                    methodologies.
+                    <br/>
+                    <br/>
+                    I then moved to another team implementing Tibco Business Works for the share brokerage and custodial
+                    services department. Again, with no formal training and only google to help me, I picked up the
+                    technology fairly rapidly. At this point, the agile methodologies, specifically the <a
+                        href="https://en.wikipedia.org/wiki/Team_software_process">disciplined agile delivery</a>
+                    methodology.
+                    <br/>
                 </v-card-text>
             </v-card>
         </v-timeline-item>
         <v-timeline-item color="cyan">
-            <span slot="opposite" class="headline font-weight-bold cyan--text">2012-Current</span>
+            <span slot="opposite" class="headline font-weight-bold cyan--text">2014-Current</span>
             <v-card class="elevation-2">
-                <v-card-title class="headline cyan--text">Standard Bank</v-card-title>
+                <v-card-title class="headline cyan--text">Standard Bank - Automation Engineer</v-card-title>
                 <v-card-text>
-                    Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod
-                    convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an
-                    salutandi sententiae.
+                    Upon my return to Standard Bank, I was asked to join the Internet Banking team. The team had fallen
+                    into disarray with no IT best practices being used. Code was all over the place, in their own
+                    branches, code was being lost, production builds were happening from developers laptops, the code
+                    that was deployed to production was not the same as the code tested, ect... <br/>
+                    <br/>
+                    I mentored and lead the team to bring them inline with best practices. Code would be committed into
+                    a branch, from there a code review was done, once accepted the code would be automatically merged to
+                    master, tagged and a nexus release created. Only official released would go into test and the same
+                    release from test would go live. <br/>
+                    <br/>
+                    I then lead a team to rebuild the Insurance System, using a Java Restful service layer with a
+                    angular front end. In around 11 months, we completely redeveloped the entire insurance system,
+                    including online quoting and policy management for a complete range of insurance products, including
+                    various combinations of motor, household, funeral, personal, professional, ect.. cover.<br/>
+                    <br/>
+                    The new insurance system infrastructure was also built completely from code. Machines were KVM's and
+                    chef was used to bootstrap and deploy the application, including app servers, web server and HA
+                    Proxies. The complete life cycle from the developers machine to production was completely automated.<br/>
+                    <br/>
+                    The next challenge was the automation of the My Updates system, which send the notifications for
+                    transactions to customers. The system ran on Z-Linux which was a challenge, since the chef cookbooks
+                    are generally not compatible with Z-Linux. A number of open source cookbooks had to be updated to
+                    work on the Z-Linux system. The next challenge was to get the chef client to work with Z-Linux, this
+                    was a collaboration between myself and the Chef developers. Once the system went live, we were the
+                    first in the work to use Chef for Z-Linux in production. <br/>
+                    <br/>
+                    From there, my focus shifted to assisting teams with their automation journeys. I started
+                    development on bots in the mannerism of HAL from 2001, a space odyssey. HAL, is a open source
+                    project written in Go, and running primarily on docker instances using kubernetes, openshift and aws
+                    fargate. The primary focus on HAL to let teams know whats happening in their systems, and alert them
+                    promptly when an anomaly is detected. To achieve the anomaly detection some custom machine learning
+                    algorithms were developed that suit the volatile nature of batch processing vs online day
+                    transactions. All messages sent from Hal, primarily to telegram, are scrutinised for language and
+                    sentiment, preventing sensitive or inappropriate messages from being sent from hal. More of the
+                    development is starting to happen using serverless constructs, such as aws lambda.<br/>
+                    <br/>
+                    Everything is build using code, primarily using Terraform to ensure that infrastructure is build
+                    from code.
                 </v-card-text>
             </v-card>
         </v-timeline-item>
